95119d24358a98e1773172c33b4c9d4a96b48009
Devren Freestone <Dev@MacBook-Pro-2.domain>
2017-07-29 09:18:51 -0700

    db setup finished

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+TRUNCATE burgers;
+
+INSERT INTO burgers (id,burger_name, devoured, DateCreated) VALUES (null,'hamburger',false,NOW());
+
+INSERT INTO burgers (id,burger_name, devoured, DateCreated) VALUES (null,'hamburger',false,NOW());
+
+-- INSERT INTO burgers (burger_name, devoured, DateCreated) VALUES 
+
+-- INSERT INTO burgers (burger_name, devoured, DateCreated) VALUES 
+
+-- INSERT INTO burgers (burger_name, devoured, DateCreated) VALUES 
+
+-- INSERT INTO burgers (burger_name, devoured, DateCreated) VALUES 
+
+-- INSERT INTO burgers (burger_name, devoured, DateCreated) VALUES 
